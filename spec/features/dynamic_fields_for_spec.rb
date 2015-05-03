f198d3bdf7308a8f06bb84e2e76182cc4798a6ca
Sergey Tokarenko <st@anahoret.com>
2015-05-03 16:41:35 +0300

    Improve tests.

--- spec/features/dynamic_fields_for_spec.rb
+++ spec/features/dynamic_fields_for_spec.rb
@@ -1,51 +1,62 @@
-describe '#dynamic_fields_for' do
-  def expect_result(user)
-    expect(page).to have_button('Update User')
-    expect(user.roles.map(&:role_name)).to match_array(['role 0', 'new role 0', 'new role 1'])
-  end
-
-  def role_inputs
-    all('[name$="[role_name]"]')
-  end
-
-  def remove_links
-    all('a', text: 'Remove role')
-  end
-
-  def deal_with_dynamic_roles
-    remove_links.last.click
-    2.times{ click_link 'Add role'}
-    remove_links.last.click
-    click_link 'Add role'
-    remove_links[1].click
-
-    role_inputs.last(2).each_with_index do |element, index|
-      element.set("new role #{index}")
+describe DynamicFieldsFor::FormHelper do
+  describe '#dynamic_fields_for' do
+    def expect_result(user)
+      expect(page).to have_button('Update User')
+      expect(user.roles.map(&:role_name)).to match_array(['role 0', 'new role 0', 'new role 1'])
     end
-  end
 
-  let(:user){User.first}
+    def role_inputs
+      all('[name$="[role_name]"]')
+    end
 
-  it 'should pass for new object' do
-    visit "/users/new"
+    def remove_links
+      all('a', text: 'Remove role')
+    end
 
-    deal_with_dynamic_roles
+    def deal_with_dynamic_roles
+      expect{remove_links.last.click}.to change{remove_links.size}.from(3).to(2)
+      expect{2.times{ click_link 'Add role'}}.to change{remove_links.size}.to(4)
+      expect{remove_links.last.click}.to change{remove_links.size}.to(3)
+      expect{click_link 'Add role'}.to change{remove_links.size}.to(4)
+      expect{remove_links[1].click}.to change{remove_links.size}.to(3)
 
-    fill_in 'user[user_name]', with: 'New user'
-    role_inputs.first.set('role 0')
+      role_inputs.last(2).each_with_index do |element, index|
+        element.set("new role #{index}")
+      end
+    end
 
-    click_button 'Create User'
+    let(:user){User.first}
 
-    expect_result(User.find(URI.parse(current_url).path.match(/users\/(\d+)\/edit/)[1]))
-  end
+    %w(form_for).each do |form_type|
+      context form_type do
 
-  it 'should pass for existing object' do
-    visit "/users/#{user.id}/edit"
+        it 'should pass for new object' do
+          visit "/users/new"
 
-    deal_with_dynamic_roles
+          within("##{form_type}") do
+            deal_with_dynamic_roles
 
-    click_button 'Update User'
+            fill_in 'user[user_name]', with: 'New user'
+            role_inputs.first.set('role 0')
 
-    expect_result(user)
+            click_button 'Create User'
+          end
+
+          expect_result(User.find(URI.parse(current_url).path.match(/users\/(\d+)\/edit/)[1]))
+        end
+
+        it 'should pass for existing object' do
+          visit "/users/#{user.id}/edit"
+
+          within("##{form_type}") do
+            deal_with_dynamic_roles
+
+            click_button 'Update User'
+          end
+
+          expect_result(user)
+        end
+      end
+    end
   end
 end
