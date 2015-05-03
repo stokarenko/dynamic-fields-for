5a19a5578c9547f552375725978b439224148013
Sergey Tokarenko <st@anahoret.com>
2015-05-03 09:38:48 +0300

    Use webkit.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
+
+  Capybara.default_driver = :webkit
 end
--- spec/support/database.rb
+++ spec/support/database.rb
@@ -9,3 +9,13 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.global_fixtures = :all
 end
+
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
