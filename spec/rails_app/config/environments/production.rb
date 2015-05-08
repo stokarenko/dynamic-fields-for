c3a4340d4ba47dfaff24c38b94a4daccbe20c6a6
Sergey Tokarenko <st@anahoret.com>
2015-05-08 21:48:06 +0300

    Remove Rails 4.2.1 deprecation warnings.

--- spec/rails_app/config/environments/production.rb
+++ spec/rails_app/config/environments/production.rb
@@ -20,7 +20,9 @@ RailsApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  DynamicFieldsFor.rails4? ?
+    config.serve_static_files = false :
+    config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
